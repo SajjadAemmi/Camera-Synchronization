69773544dd9b47f9e577af127c74432829497d04
Sajjad Aemmi <sajjadaemmi@gmail.com>
2024-10-08 09:20:47 +0330

    f string bug fixed

--- client.py
+++ client.py
@@ -18,7 +18,7 @@ UPLOAD_ENDPOINT = "/upload"
 sio = socketio.Client()
 
 camera_index = args.video
-output_dir = './videos'
+output_dir = "./videos"
 
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
@@ -39,7 +39,7 @@ def apply_camera_settings(settings):
         cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
         cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
         cap.set(cv2.CAP_PROP_FPS, fps)
-        print(f"Camera settings applied: {width}x{height} @ {fps} FPS")
+        print("Camera settings applied:", width, "x", height, "fps:", fps)
 
 
 def start_recording():
@@ -47,19 +47,22 @@ def start_recording():
     if not is_recording:
         timestamp = time.strftime("%Y%m%d-%H%M%S")
         recording_file_path = os.path.join(
-            output_dir, f"recording_{timestamp}_{args.video}_.mp4")
-        timestamp_file_path = os.path.join(output_dir, f"timestamps_{
-                                           timestamp}__{args.video}.txt")
+            output_dir, "recording_" + timestamp + "_" + str(args.video) + "_.mp4"
+        )
+        timestamp_file_path = os.path.join(
+            output_dir, "recording_" + timestamp + "_" + str(args.video) + "_.txt"
+        )
         timestamps = []
 
-        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+        fourcc = cv2.VideoWriter_fourcc(*"mp4v")
         frame_width = int(cap.get(3))
         frame_height = int(cap.get(4))
         fps = int(cap.get(cv2.CAP_PROP_FPS))
         video_writer = cv2.VideoWriter(
-            recording_file_path, fourcc, 25, (frame_width, frame_height))
+            recording_file_path, fourcc, 25, (frame_width, frame_height)
+        )
         is_recording = True
-        print(f"Recording started: {recording_file_path}")
+        print("Recording started:", recording_file_path)
 
 
 def stop_recording():
@@ -67,53 +70,54 @@ def stop_recording():
     if is_recording:
         is_recording = False
         video_writer.release()
-        print(f"Recording stopped and saved to {recording_file_path}")
+        print("Recording stopped and saved to", recording_file_path)
 
         # Save timestamps to the file
-        with open(timestamp_file_path, 'w') as f:
+        with open(timestamp_file_path, "w") as f:
             for ts in timestamps:
-                f.write(f"{ts}\n")
-        print(f"Timestamps saved to {timestamp_file_path}")
+                f.write(str(ts) + "\n")
+        print("Timestamps saved to", timestamp_file_path)
 
         # Upload both video and timestamp files to the server
         upload_video_and_timestamps(recording_file_path, timestamp_file_path)
 
 
 def upload_video_and_timestamps(video_path, timestamp_path):
-    with open(video_path, 'rb') as video_file, open(timestamp_path, 'rb') as timestamp_file:
+    with open(video_path, "rb") as video_file, open(
+        timestamp_path, "rb"
+    ) as timestamp_file:
         files = {
-            'video': (os.path.basename(video_path), video_file),
-            'timestamps': (os.path.basename(timestamp_path), timestamp_file)
+            "video": (os.path.basename(video_path), video_file),
+            "timestamps": (os.path.basename(timestamp_path), timestamp_file),
         }
         response = requests.post(SERVER_URL + UPLOAD_ENDPOINT, files=files)
         if response.status_code == 200:
-            print(f"Video and timestamps uploaded successfully.")
+            print("Video and timestamps uploaded successfully.")
         else:
-            print(f"Failed to upload files. Status code: {
-                  response.status_code}")
+            print("Failed to upload files. Status code:", response.status_code)
 
 
 @sio.event
 def connect():
-    print('Connection established')
+    print("Connection established")
 
 
 @sio.event
 def disconnect():
-    print('Disconnected from server')
+    print("Disconnected from server")
 
 
-@sio.on('camera_settings')
+@sio.on("camera_settings")
 def on_camera_settings(data):
     apply_camera_settings(data)
 
 
-@sio.on('start_recording')
+@sio.on("start_recording")
 def on_start_recording():
     start_recording()
 
 
-@sio.on('stop_recording')
+@sio.on("stop_recording")
 def on_stop_recording():
     stop_recording()
 
@@ -129,8 +133,8 @@ def capture_video():
                 timestamp = time.time()
                 timestamps.append(timestamp)
 
-            cv2.imshow('Webcam Feed', frame)
-            if cv2.waitKey(100) & 0xFF == ord('q'):
+            cv2.imshow("Webcam Feed", frame)
+            if cv2.waitKey(100) & 0xFF == ord("q"):
                 break
 
 
